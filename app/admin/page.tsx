f293c83d757438d0ac639615cc7ea952457307dc
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-06-02 20:43:50 -0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,21 @@
 'use client'
+import { useEffect } from "react"
 import AdminForm from "../../components/AdminForm"
 import Header from "../../components/HeaderDetails"
 
 export default function AdminPage() {
+
+  useEffect(() => {
+    window.location.href = '/'
+  }, [])
+
   return (
     <>
     <Header/>
     <div className="flex flex-col gap-4 max-w-[600px] min-h-[705px] w-full mx-auto p-4">
-      <h1 className="text-2xl font-bold">Painel Admin</h1>
+      {/* <h1 className="text-2xl font-bold">Painel Admin</h1> */}
       {/* <AdminForm /> */}
+      Not Found
     </div>
     </>
   )
