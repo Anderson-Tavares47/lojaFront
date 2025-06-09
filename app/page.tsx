46f7269c8af7f67c45faca520c6f2eff10bb2127
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-06-09 00:44:31 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@
 import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import ProductCard from '../components/ProductCard'
+import Spinner from '../components/Load'
 import { Product } from '../lib/types'
 
 export default function HomePage() {
   const [products, setProducts] = useState<Product[]>([])
   const [search, setSearch] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -18,6 +20,8 @@ export default function HomePage() {
         setProducts(data)
       } catch (err) {
         console.error(err)
+      } finally {
+        setLoading(false)
       }
     }
 
@@ -26,24 +30,30 @@ export default function HomePage() {
 
   const filteredProducts = products.filter(
     (product) =>
-      product &&
-      product.name?.toLowerCase().includes(search.toLowerCase())
+      product && product.name?.toLowerCase().includes(search.toLowerCase())
   )
 
   return (
     <>
       <Header search={search} onSearchChange={setSearch} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-5 min-h-[705px]">
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product) => (
-            product?.id && (
-              <ProductCard key={product.id} product={product} />
+
+      {/* Spinner fora da grid */}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-5 min-h-[705px]">
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map(
+              (product) =>
+                product?.id && (
+                  <ProductCard key={product.id} product={product} />
+                )
             )
-          ))
-        ) : (
-          <p className="text-center col-span-full">Nenhum produto encontrado.</p>
-        )}
-      </div>
+          ) : (
+            <p className="text-center col-span-full">Nenhum produto encontrado.</p>
+          )}
+        </div>
+      )}
     </>
   )
 }
